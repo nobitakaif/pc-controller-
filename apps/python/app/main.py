1531e4c4fb015e88cafbc3b66073db27dc055ae8
mohd kaif <mk2818356@gmail.com>
2026-09-17 01:43:10 +0530

    finished tool calling from routes

--- apps/python/app/main.py
+++ apps/python/app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from agent_tools import create_folder
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
+    result = create_folder.invoke({
+        "folder_name" : "chh" 
+    })
     return {
-        "message" : "alright"
+        "message" : "alright",
+        "result" : result
     }
